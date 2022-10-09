f02a25f3128a472540cc0e16be322850cc3aa46f
OMGxItzCheezy <hectoralvarez578@gmail.com>
2022-10-09 18:04:40 -0500

    Done

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -12,7 +12,7 @@ const homeview = (req, res, next) => {
 
 const generatePdf = async (req, res, next) => {
         const html = fs.readFileSync(path.join(__dirname, '../views/template.html'), 'utf-8');
-        const filename = Math.random() + '_doc' + '.pdf';
+        const filename = (Math.floor(Math.random()*1000)) + '_doc' + '.pdf';
         let array = [];
 
         data.forEach(d => {
